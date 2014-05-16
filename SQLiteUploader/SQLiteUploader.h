e1557842e7a3ebd0d709db1c25f8c76a8d0e79c7
Armand van der Walt <amd@yotta.co.za>
2014-05-16 18:38:13 +0200

    Initial Commit

---
+++ SQLiteUploader/SQLiteUploader.h
@@ -0,0 +1,13 @@
+//
+//  SQLiteUploader.h
+//  SQLiteUploader
+//
+//  Created by Armand Van der Walt on 2014/05/16.
+//  Copyright (c) 2014 Yotta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SQLiteUploader : NSObject
+
+@end
---
+++ SQLiteUploader/SQLiteUploader.m
@@ -0,0 +1,13 @@
+//
+//  SQLiteUploader.m
+//  SQLiteUploader
+//
+//  Created by Armand Van der Walt on 2014/05/16.
+//  Copyright (c) 2014 Yotta. All rights reserved.
+//
+
+#import "SQLiteUploader.h"
+
+@implementation SQLiteUploader
+
+@end
